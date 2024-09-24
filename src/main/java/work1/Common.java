9d14b2e9ca5a69dccf99232aa07952e67ba2626f
jiangshuai <937261987@qq.com>
2024-09-24 17:41:18 +0800

    second commit

--- src/main/java/work1/Common.java
+++ src/main/java/work1/Common.java
@@ -1,5 +1,7 @@
 package work1;
 
+import java.util.Random;
+
 /**
  * @Title Common
  * @Author Administrator
@@ -8,4 +10,49 @@ package work1;
  * @Version 1.0
  **/
 public class Common {
+    static Random r = new Random();
+    public static void main(String[] args) {
+        char[]arr = createAlpha();
+        int num = createNum();
+        String str = createVatifaction(arr,num);
+        str = toRandom(str);
+        System.out.println(str);
+    }
+    public static char[] createAlpha(){
+        char[] arr = new char[52];
+        for (int i = 0; i < arr.length; i++) {
+            if(i<26){
+                arr[i] = (char)('a' + i);
+            }
+            else
+                arr[i] = (char)('A'+ i -26);
+        }
+        return arr;
+    }
+    public static int createNum(){
+        int x = r.nextInt(10);
+        return x;
+    }
+    public static String createVatifaction(char[]arr,int num){
+        String str = "";
+        for (int i = 0; i < 4; i++) {
+            int index = r.nextInt(arr.length);
+            str += arr[index];
+        }
+        str += num;
+        return str;
+    }
+    public static String toRandom(String str){
+        char[]arr = str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            char temp = ' ';
+            int index = r.nextInt(arr.length);
+            temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+        String newstr = new String(arr);
+        return newstr;
+    }
+
 }
