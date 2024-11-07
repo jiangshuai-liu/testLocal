bf69a5fd6c6cf8980519041ad24e10ba12be7589
jiangshuai <937261987@qq.com>
2024-11-07 10:12:16 +0800

    日常提交20241107

--- src/main/java/work1/MongoDB.java
+++ src/main/java/work1/MongoDB.java
@@ -9,7 +9,10 @@ import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
 import org.bson.Document;
 
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Title MongoDB
@@ -19,6 +22,7 @@ import java.util.List;
  * @Version 1.0
  **/
 public class MongoDB {
+
     public static void main(String[] args) {
         // 创建 MongoDB 连接
         MongoClient mongo = new MongoClient("localhost", 27017);
@@ -34,16 +38,42 @@ public class MongoDB {
         // 检索集合
         MongoCollection<Document> collection = database.getCollection("myInfo");
         System.out.println("chose collection : " + collection.getNamespace());
+
         //获得全部集合
-        //listCollectionNames(database);
+        listCollectionNames(database);
         //查询数据
-        //find(collection);
+        find(collection);
         //新增数据
         //insertOne(collection);
+        //新增多个数据
+        //insertMany(collection,1);
         //更新数据
         //updateMany(collection);
-        //createAlpha();
+        //随机查询
+        selectUseRandomCode(collection);
+        //删除多个数据
+        //deleteMany(collection);
+    }
 
+
+    /**
+     * 随机码查询
+     * @param collection
+     */
+    private static void selectUseRandomCode (MongoCollection<Document> collection){
+        int i=0;
+        while (true){
+            i++;
+            String str =  Common.getRandomCode(3);
+            Document document = new Document("description",str);
+            boolean bool=find(collection,document);
+            if(i%100000==0){
+                System.out.println(i);
+            }
+            if(bool){
+                break;
+            }
+        }
     }
 
 
@@ -89,16 +119,8 @@ public class MongoDB {
      * 数据新增(多个数据 insertMany)
      * @param collection
      */
-    private static void insertMany(MongoCollection<Document> collection, List <Document> list){
-
-        Document document = new Document("title", "002")
-                .append("description", "database")
-                .append("likes", 164)
-                .append("url", "https://www.runoob.com/mongodb/mongodb")
-                .append("by", "jiangshuai");
-
-        // 将文档插入到集合中
-        collection.insertMany(list);
+    private static void insertMany(MongoCollection<Document> collection, int length){
+        write(collection,length);
         System.out.println("Document insert successfully!");
     }
 
@@ -113,6 +135,20 @@ public class MongoDB {
         for (Document document : iterDoc) {
             System.out.println(document);
         }
+    }
+    /**
+     * 查询数据
+     * @param collection
+     */
+    private static Boolean find(MongoCollection<Document> collection,Document document){
+        // 获取 iterable 对象
+        FindIterable<Document> iterDoc = collection.find(document);
+        // 获取迭代器
+        for (Document getDocument : iterDoc) {
+            System.out.println(getDocument);
+            return true;
+        }
+        return false;
     }
     /**
      * 数据更新（多条更新 updateMany）
@@ -146,7 +182,35 @@ public class MongoDB {
      * @param collection
      */
     private static void deleteMany(MongoCollection<Document> collection){
-        collection.deleteMany(Filters.eq("title", "MongoDB"));
+        collection.deleteMany(Filters.eq("by", "jiangshuai"));
         System.out.println("Document delete successfully...");
+    }
+
+    public static void write(MongoCollection<Document> collection, int length) {
+        ArrayList writes = new ArrayList();
+        for(int num=0;num<length;num++) {
+            Document document = new Document("title", num)
+                    .append("description", Common.getRandomCode(3))
+                    .append("likes", Common.getRandomCode(8))
+                    .append("url", "https://www.runoob.com/mongodb/mongodb")
+                    .append("by", "jiangshuai");
+
+            writes.add(document);
+        }
+        collection.insertMany(writes);
+    }
+
+    public static Map<String, Object> objectToMap(Object obj) {
+        HashMap map = new HashMap();
+        try {
+            Class<?> clazz = obj.getClass();
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                map.put(field.getName(), field.get(obj));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return map;
     }
 }
